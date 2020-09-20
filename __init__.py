d26560a9ff05cd1dfd75cb77cb33737dc45a291a
Dmitriy Levchenko <levchenko.d.a1998@gmail.com>
2020-09-21 02:39:16 +0300

    update

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-__all__ = ('functions',)
+from MixIn.functools import *
--- functions.py
+++ functions.py
@@ -1,15 +1,6 @@
 """Various functions"""
 import time
 from functools import wraps, partial
-from fake_headers.headers import make_header
-from fake_useragent import UserAgent
-
-
-def make_headers():
-    """Make fake headers"""
-    headers = make_header()
-    headers['user-agent'] = UserAgent().chrome
-    return headers
 
 
 def timer(func=None, start_print=False):
