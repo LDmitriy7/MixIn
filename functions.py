272b510841d5f701c7787664b23664cf038d51fa
Dmitriy <levchenko.d.a1998@gmail.com>
2021-04-17 06:39:25 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,7 +3,7 @@ import time
 from functools import wraps, partial
 
 
-def timer(func=None, start_print=False, print_=print):
+def timer(func=None, start_print=False, printer=print):
     """Засекает время выполнения функции, по желанию уведомляет о начале выполнения"""
     if func is None:
         return partial(timer, start_print=start_print)
@@ -11,10 +11,10 @@ def timer(func=None, start_print=False, print_=print):
     @wraps(func)  # придает новой функции документацию, имя и сигнатуру исходной
     def wrapper(*args, **kwargs):
         if start_print:
-            print_(f'>> func "{func.__name__}" -> started')
+            printer(f'>> func "{func.__name__}" -> started')
         start_time = time.time()
         result = func(*args, **kwargs)
-        print_(f'>> func "{func.__name__}" -> finish in {time.time() - start_time:.8f}s')
+        printer(f'>> func "{func.__name__}" -> finish in {time.time() - start_time:.8f}s')
         return result
 
     return wrapper
